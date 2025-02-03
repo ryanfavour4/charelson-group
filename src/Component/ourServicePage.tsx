dc01fe694a5aa8c9f9003e85d74f0d4ab447ae8c
jehovah-son <chukwukasuccess0@gmail.com>
2025-02-03 22:03:34 +0100

    complete

---
+++ src/Component/ourServicePage.tsx
@@ -0,0 +1,12 @@
+export default function OurService() {
+  return (
+    <div>
+      <div className="our-service">
+        <h3>Our services</h3>
+        <h2>
+          Explore Our <span>Services</span>
+        </h2>
+      </div>
+    </div>
+  );
+}
